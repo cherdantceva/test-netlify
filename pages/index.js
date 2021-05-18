19ad8bdc379be00bd2173d92e05ba3845bb5dec9
Татьяна Антоновна Черданцева <ta_cherdantseva@bingo-boom.com>
2021-05-18 20:06:31 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import '../scss/common.scss'
+
+import Donation from '../components/Donation'
+
+const Index = () => {
+
+    return (
+        <Donation />
+    )
+}
+
+export default Index
