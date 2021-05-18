6fcceb05da9fd40cd689c733db0bb6747f9caf39
Татьяна Антоновна Черданцева <ta_cherdantseva@bingo-boom.com>
2021-05-18 20:24:02 +0300

    netlify add

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
-// import '../scss/common.scss'
+import Head from "next/head"
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
 
-import Donation from '../components/Donation'
-
-const Index = () => {
-
-    return (
-        <Donation />
-    )
+export default class Home extends Component {
+    render() {
+        let { title, cats } = attributes;
+        return (
+            <>
+                <Head>
+                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+                </Head>
+                <article>
+                    <h1>{title}</h1>
+                    <HomeContent />
+                    <ul>
+                        {cats.map((cat, k) => (
+                            <li key={k}>
+                                <h2>{cat.name}</h2>
+                                <p>{cat.description}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </article>
+            </>
+        )
+    }
 }
-
-export default Index
