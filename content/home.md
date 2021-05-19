b1c347edeab352b321c186333b75e09788323abf
Татьяна Антоновна Черданцева <ta_cherdantseva@bingo-boom.com>
2021-05-19 19:28:28 +0300

    add features

--- content/home.md
+++ content/home.md
@@ -1,13 +1,11 @@
 ---
-top: 
-    title: Awesome kitties
-    content: lalalalalalala
+title: Awesome kitties
+content: 2019-03-17T19:31:20.591Z
 features:
-- title: Модель эндаумента
-  text: На добрые дела идет только инвестиционный доход от взноса, а его "тело" не тратится, поэтому он работает всегда. Кстати, средняя доходность ФЦК МФТИ с 2016 года — 16% годовых в рублях.
-- title: Натуральная форма
-  text: Студенты получат не деньги, а конкретные блага, которые оптом закупает эндаумент. Это дает ему более высокую покупательную способность и гарантирует целевое использование взносов.
+- description: 'Maru is a Scottish Fold from Japan, and he loves boxes.'
+  name: Maru 
+- description: Lil Bub is an American celebrity cat known for her unique appearance.
+  name: Lil Bub
+- description: 'Grumpy cat is an American celebrity cat known for her grumpy appearance.'
+  name: Grumpy cat 
 ---
-Welcome to my awesome page about cats of the internet.
-
-This page is built with NextJS, and content is managed in Netlify CMS
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { attributes} from '../content/home.md';
 import HomeTop from "../components/HomeTop";
 export default class Home extends Component {
     render() {
-        let {top, features} = attributes;
+        let {title, content, features} = attributes;
         console.log(attributes)
         return (
             <>
@@ -13,15 +13,15 @@ export default class Home extends Component {
                     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
                 </Head>
                 <HomeTop
-                    title={top.title}
-                    content={top.content}
+                    title={title}
+                    content={content}
                 />
                 <div className={'features'}>
                     <div className={'items'}>
                         {features.map((item) => (
-                            <div key={item.title}>
-                                <h2>{item.title}</h2>
-                                <p>{item.text}</p>
+                            <div key={item.name}>
+                                <h2>{item.name}</h2>
+                                <p>{item.description}</p>
                             </div>
                         ))}
                     </div>
